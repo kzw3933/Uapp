2ed731d46038ac0c87835e81f24a1e0633e52dde
kzw3933 <3099097649@qq.com>
2023-05-04 13:32:32 +0800

    添加本地测试用客户端

---
+++ tests/client.py
@@ -0,0 +1,33 @@
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from urpc import UappService
+from io import BytesIO
+from PIL import Image
+from urpc.ttypes import *
+
+# 创建 Thrift 客户端
+transport = TSocket.TSocket('localhost', 7860) # 创建 TSocket 对象，指定服务器的地址和端口
+transport = TTransport.TBufferedTransport(transport) # 创建 TTransport 对象，用于封装 TSocket 对象
+protocol = TBinaryProtocol.TBinaryProtocol(transport) # 创建 TProtocol 对象，用于序列化和反序列化数据
+client = UappService.Client(protocol) # 创建 ExampleService.Client 对象，用于调用远程接口
+
+# 打开连接
+transport.open()
+
+# 调用远程接口
+register_info = RegisterInfo('PB20061338', 'kzw1338', '23982092040', '123456')
+client.register(register_info)
+img = Image.open('design.png')
+buffered = BytesIO()
+img.save(buffered, format='PNG')
+img_byte = buffered.getvalue()
+for i in range(20):
+    postinfo = PostInfo('PB20061338', True, img_byte, 'test'+str(i), 'test', 'test', True, 12682379484, "test", 1628372838)
+    client.uploadPost(postinfo)
+# result = client.getPostBy10()
+# for i in result:
+#     print(i)
+
+# 关闭连接
+transport.close()
